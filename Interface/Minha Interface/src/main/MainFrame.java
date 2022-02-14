d2de16e380fc97d9f4684f6452278e75d975659d
Lucio746 <82363146+Lucio746@users.noreply.github.com>
2022-02-14 13:54:14 -0300

    Primeiro Commit
    Começo de Projeto

---
+++ Interface/Minha Interface/src/main/MainFrame.java
@@ -0,0 +1,49 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField tfOi;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 586, 412);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		tfOi = new JTextField();
+		tfOi.setBounds(249, 156, 94, 40);
+		tfOi.setText("Ol\u00E1 mundo!");
+		contentPane.add(tfOi);
+		tfOi.setColumns(10);
+	}
+}
